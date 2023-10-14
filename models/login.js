1985d6c4d08e46c2d206121f77a4132ae29ed245
Jeedeexyz <123000633+Jeedeexyz@users.noreply.github.com>
2023-10-14 13:10:07 +0500

    Added Super Admin in enum of schema so we can give the full control to the Super Admin to add people and give them the roles as he wish and demote the role as well

--- models/login.js
+++ models/login.js
@@ -25,7 +25,7 @@ const userSchema = new mongoose.Schema({
   },
   role: {
     type : String,
-    enum: ["Admin","Manager","Customer","Leader"],
+    enum: ["Admin","Manager","Customer","Leader","Super Admin"],
     default: "Customer",
   }
 });
