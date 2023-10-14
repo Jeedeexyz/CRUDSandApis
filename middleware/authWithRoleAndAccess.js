a402487bf5220ab7306369c54c85b6fef764bcb5
Jeedeexyz <123000633+Jeedeexyz@users.noreply.github.com>
2023-10-14 18:35:34 +0500

    The middle ware to check the user is super admin or not

---
+++ middleware/authWithRoleAndAccess.js
@@ -0,0 +1,35 @@
+const jwt = require("jsonwebtoken");
+
+
+const authWithRole = () => (req, res, next) => {
+  try {
+   req.headers.authorization && 
+    req.headers.authorization.startsWith("Bearer");
+
+    let token = req.headers.authorization.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({ message: "Access Denied, token missing" });
+    }
+    jwt.verify(token, process.env.PRIVATE_KEY, (err, user) => {
+      if (err) {
+        return res.status(403).json({ message: "Invalid Token" });
+      }
+
+      if (user.role === "Super Admin") {
+      
+        next();
+
+      } else {
+        return res.status(403).json({ message: "Permissions denied!" }); 
+      }
+      
+    });
+  } catch (error) {
+    res.status(500).json({ error:error.message });
+  }
+};
+
+
+
+module.exports = authWithRole;
